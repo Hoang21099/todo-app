fb71f3e0c9d26e374dffa0bbf6d09fd111f15ac1
Hoang21099 <hoang21099@gmail.com>
2020-02-07 15:22:29 +0700

    Updates

---
+++ precache-manifest.ab47a2207e3ef6df99bf38c82122dbb4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf385775c0c6b69e513f6d039f932fd",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "658bf4f2df6437337ff8",
+    "url": "/todo-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d568dce15f2d62387f7f",
+    "url": "/todo-app/static/css/main.777d6138.chunk.css"
+  },
+  {
+    "revision": "658bf4f2df6437337ff8",
+    "url": "/todo-app/static/js/2.ca7cf712.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/todo-app/static/js/2.ca7cf712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d568dce15f2d62387f7f",
+    "url": "/todo-app/static/js/main.42423615.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  },
+  {
+    "revision": "2779a2798b0570a5476363ac5892f515",
+    "url": "/todo-app/static/media/2.2779a279.jpg"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/todo-app/static/media/AntDesign.3a2ba315.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/todo-app/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/todo-app/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/todo-app/static/media/Feather.6beba7e6.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-app/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/todo-app/static/media/Foundation.e20945d7.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/todo-app/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "d15c1216957060fac577af6151fb8cfe",
+    "url": "/todo-app/static/media/MaterialCommunityIcons.d15c1216.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/todo-app/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "73b8cff012825060b308d2162f31dbb2",
+    "url": "/todo-app/static/media/Octicons.73b8cff0.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/todo-app/static/media/SimpleLineIcons.d2285965.ttf"
+  },
+  {
+    "revision": "5cdf883b18a5651a29a4d1ef276d2457",
+    "url": "/todo-app/static/media/Zocial.5cdf883b.ttf"
+  },
+  {
+    "revision": "8ac683ee799698e6ee5cef695386486c",
+    "url": "/todo-app/static/media/bg_header.8ac683ee.jpg"
+  }
+]);
